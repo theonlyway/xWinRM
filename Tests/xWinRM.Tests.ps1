d36b5faaff48544a3f1820d5e5184e602225c642
Anthony <anthonywaye@gmail.com>
2017-08-08 20:23:00 +1000

    Test and logic updates

--- Tests/xWinRM.Tests.ps1
+++ Tests/xWinRM.Tests.ps1
@@ -7,6 +7,30 @@ $Global:DSCModuleName      = 'xWinRM'
 $Global:DSCResourceName    = 'xWinRM'
 
 InModuleScope -ModuleName xWinRM -ScriptBlock {
+  $returnValue = @{
+    Service_Basic              = 'false'
+    Client_Basic               = 'false'
+    Client_Digest              = 'false'
+    Service_Kerberos           = 'false'
+    Client_Kerberos            = 'false'
+    Service_Negotiate          = 'false'
+    Client_Negotiate           = 'false'
+    Service_Certificate        = 'true'
+    Client_Certificate         = 'false'
+    Service_CredSSP            = 'true'
+    Client_CredSSP             = 'true'
+    Service_AllowUnencrypted   = 'true'
+    Client_AllowUnencrypted    = 'true'
+    HttpPort                   = 5985
+    HttpsPort                  = 5986
+    MaxEnvelopeSizekb          = 500
+    MaxTimeoutms               = 60000
+    MaxBatchItems              = 32000
+    MaxProviderRequests        = 4294967295
+    MaxMemoryPerShellMB        = 1024
+    CurrentHTTPSCertThumpprint = $null
+  }
+
   Describe -Name "Testing if $($Global:DSCResourceName)\Get-TargetResource and $($Global:DSCResourceName)\Test-TargetResource return correct objects" -Fixture {
     It -name 'Get-TargetResource returns a hashtable' -test {
       Get-TargetResource -Protocol HTTP -Ensure Present | Should Be 'System.Collections.Hashtable'
@@ -21,7 +45,7 @@ InModuleScope -ModuleName xWinRM -ScriptBlock {
       It -name 'Configuring WinRM HTTP listener' -test {
         Set-TargetResource -Protocol HTTP -Ensure Present -HttpPort 6000 -Service_AllowUnencrypted false
       }
-      It -name 'Configuring WinRM HTTSP listener' -test {
+      It -name 'Configuring WinRM HTTPS listener' -test {
         Set-TargetResource -Protocol HTTPS -Ensure Present -Service_AllowUnencrypted false -HttpsPort 6005
       }
     }
@@ -76,6 +100,39 @@ InModuleScope -ModuleName xWinRM -ScriptBlock {
       It -name 'Test-TargetResource returns true' -test {
         Test-TargetResource -Protocol HTTPS -Ensure Present -Service_AllowUnencrypted false -HttpsPort 6005 | Should Be $true
       }
+    }
+    Context -Name "Testing $($Global:DSCResourceName)\Set-TargetResource logic for ensure = absent" -Fixture {   
+      It -name 'Configuring WinRM HTTP listener' -test {
+        Set-TargetResource -Protocol HTTP -Ensure Absent -HttpPort 6000 -Service_AllowUnencrypted false
+      }
+      It -name 'Configuring WinRM HTTPS listener' -test {
+        Set-TargetResource -Protocol HTTPS -Ensure Absent -Service_AllowUnencrypted false -HttpsPort 6005
+      }
+    }
+    Context -Name "Testing $($Global:DSCResourceName)\Test-TargetResource results" -Fixture {   
+      It -name 'Test-TargetResource returns true' -test {
+        Test-TargetResource -Protocol HTTP -Ensure Absent -Service_AllowUnencrypted false -HttpPort 6000 | Should Be $true
+      }
+      It -name 'Test-TargetResource returns true' -test {
+        Test-TargetResource -Protocol HTTPS -Ensure Absent -Service_AllowUnencrypted false -HttpsPort 6005 | Should Be $true
+      }
+    }
+  }
+  Describe -Name "$($Global:DSCResourceName)\Test-TargetResource" -Fixture {
+    Mock -CommandName Get-TargetResource -MockWith {
+      $returnValue
+    }
+    It -name 'Test-TargetResource returns false' -test {
+      Test-TargetResource -Protocol HTTP -Ensure Present -Service_AllowUnencrypted false -HttpPort 6000 | Should Be $false
+    }
+    It -name 'Test-TargetResource returns false' -test {
+      Test-TargetResource -Protocol HTTPS -Ensure Present -Service_AllowUnencrypted false -HttpsPort 6005 | Should Be $false
+    }
+    It -name 'Test-TargetResource returns true' -test {
+      Test-TargetResource -Protocol HTTP -Ensure Absent -Service_AllowUnencrypted false -HttpPort 6000 | Should Be $true
+    }
+    It -name 'Test-TargetResource returns true' -test {
+      Test-TargetResource -Protocol HTTPS -Ensure Absent -Service_AllowUnencrypted false -HttpsPort 6005 | Should Be $true
     }
   }
 }
